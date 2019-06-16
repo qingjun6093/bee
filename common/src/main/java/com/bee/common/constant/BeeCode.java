e07f3aa91b8c1757d419fa78d508f4551f56121b
qingjun6093 <848326806@qq.com>
2019-06-16 11:44:31 +0800

    1.搭建项目骨架;2.创建全局响应体BeeResp.java

---
+++ common/src/main/java/com/bee/common/constant/BeeCode.java
@@ -0,0 +1,16 @@
+package com.bee.common.constant;
+
+/**
+ * @author jjq
+ * @version 1.0
+ * @date 2019/6/16
+ * @desc 返回码
+ */
+public interface  BeeCode {
+
+    int SUCCESS = 0;
+
+    int PARAM_INVALID = 2;
+
+
+}
---
+++ common/src/main/java/com/bee/common/constant/BeeMsg.java
@@ -0,0 +1,14 @@
+package com.bee.common.constant;
+
+/**
+ * @author jjq
+ * @version 1.0
+ * @date 2019/6/16
+ * @desc 返回码描述
+ */
+public interface BeeMsg {
+
+    String SUCCESS = "success";
+
+    String PARAM_INVALID = "param invalid";
+}
